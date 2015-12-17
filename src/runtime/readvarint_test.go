182d2aef76795fe5692618226ceff0a1b65d30ec
Nick Terry <j.nick.terry@gmail.com>
2015-12-17 00:24:00 -0800

    More goofy opts

--- src/runtime/readvarint_test.go
+++ src/runtime/readvarint_test.go
@@ -2,8 +2,8 @@ package readvarint_test
 
 import (
 	"encoding/binary"
+	"math/rand"
 	"testing"
-  "math/rand"
 )
 
 func lib(p []byte) (newp []byte, val uint32) {
@@ -28,30 +28,30 @@ func plain(p []byte) (newp []byte, val uint32) {
 func clean(p []byte) (newp []byte, val uint32) {
 	var v uint32
 
-  v |= uint32(p[0]&0x7F) << (0 * 7)
-  if p[0] < 128 {
-    return p[0+1:], v
-  }
+	v |= uint32(p[0]&0x7F) << (0 * 7)
+	if p[0] < 128 {
+		return p[0+1:], v
+	}
 
-  v |= uint32(p[1]&0x7F) << (1 * 7)
-  if p[1] < 128 {
-    return p[1+1:], v
-  }
+	v |= uint32(p[1]&0x7F) << (1 * 7)
+	if p[1] < 128 {
+		return p[1+1:], v
+	}
 
-  v |= uint32(p[2]&0x7F) << (2 * 7)
-  if p[2] < 128 {
-    return p[2+1:], v
-  }
+	v |= uint32(p[2]&0x7F) << (2 * 7)
+	if p[2] < 128 {
+		return p[2+1:], v
+	}
 
-  v |= uint32(p[3]&0x7F) << (3 * 7)
-  if p[3] < 128 {
-    return p[3+1:], v
-  }
+	v |= uint32(p[3]&0x7F) << (3 * 7)
+	if p[3] < 128 {
+		return p[3+1:], v
+	}
 
-  v |= uint32(p[4]&0x7F) << (4 * 7)
-  if p[4] < 128 {
-    return p[4+1:], v
-  }
+	v |= uint32(p[4]&0x7F) << (4 * 7)
+	if p[4] < 128 {
+		return p[4+1:], v
+	}
 
 	return p[5:], v
 }
@@ -80,17 +80,61 @@ func magic(p []byte) (newp []byte, val uint32) {
 	return p[5:], v | (uint32(p[4]&0x7F) << 28)
 }
 
+func magic2(p []byte) (newp []byte, val uint32) {
+	v := uint32(p[0] & 0x7F)
+	if p[0] < 128 {
+		return p[0+1:], v
+	}
+
+	v |= uint32(p[1]&0x7F) << 7
+	if p[1] < 128 {
+		return p[1+1:], v
+	}
+
+	v |= uint32(p[2]&0x7F) << (2 * 7)
+	if p[2] < 128 {
+		return p[2+1:], v
+	}
+
+	v |= uint32(p[3]&0x7F) << (3 * 7)
+	if p[3] < 128 {
+		return p[3+1:], v
+	}
+
+	return p[4+1:], v | (uint32(p[4]&0x7F) << (4 * 7))
+}
+
+func magic3(p []byte) (newp []byte, val uint32) {
+	if p[0] < 128 {
+		return p[1:], uint32(p[0] & 0x7F)
+	}
+
+	if p[1] < 128 {
+		return p[2:], uint32(p[0]&0x7F) | uint32(p[1]&0x7F)<<7
+	}
+
+	if p[2] < 128 {
+		return p[3:], uint32(p[0]&0x7F) | uint32(p[1]&0x7F)<<7 | uint32(p[2]&0x7F)<<14
+	}
+
+	if p[3] < 128 {
+		return p[4:], uint32(p[0]&0x7F) | uint32(p[1]&0x7F)<<7 | uint32(p[2]&0x7F)<<14 | uint32(p[3]&0x7F)<<21
+	}
+
+	return p[5:], uint32(p[0]&0x7F) | uint32(p[1]&0x7F)<<7 | uint32(p[2]&0x7F)<<14 | uint32(p[3]&0x7F)<<21 | uint32(p[4]&0x7F)<<28
+}
+
 var randBytes []byte
 var randInts []uint32
 
 func init() {
-  var scratch [5]byte
-  for i:=0; i<1000; i++ {
-    x := rand.Uint32()
-    randInts = append(randInts, x)
-    l := binary.PutUvarint(scratch[:], uint64(x))
-    randBytes = append(randBytes, scratch[:l]...)
-  }
+	var scratch [5]byte
+	for i := 0; i < 1000; i++ {
+		x := rand.Uint32()
+		randInts = append(randInts, x)
+		l := binary.PutUvarint(scratch[:], uint64(x))
+		randBytes = append(randBytes, scratch[:l]...)
+	}
 }
 
 func ints2varint(ints []uint32) []byte {
@@ -114,6 +158,8 @@ func TestReadVarint(t *testing.T) {
 		{"plain", plain},
 		{"clean", clean},
 		{"magic", magic},
+		{"magic2", magic2},
+		{"magic3", magic3},
 	}
 
 	ints := []uint32{0, 1, 2, 3, 4, 5, 0xFE, 0xFF, 0x100, 0xFFFE, 0xFFFF, 0x10000, 0xFFFFFFFF}
@@ -121,8 +167,9 @@ func TestReadVarint(t *testing.T) {
 	bytes := ints2varint(ints)
 
 	for _, tt := range data {
-    b := bytes
-		for _, want := range ints { var got uint32 
+		b := bytes
+		for _, want := range ints {
+			var got uint32
 			b, got = tt.f(b)
 			if got != want {
 				t.Errorf("%s: %d(%x) != %d(%x)", tt.name, want, want, got, got)
@@ -132,53 +179,79 @@ func TestReadVarint(t *testing.T) {
 }
 
 func BenchmarkLib(b *testing.B) {
-  for i:=0; i<b.N; i++ {
-    bytes := randBytes
-    for _, want := range(randInts) {
-      var got uint32
-      bytes, got = lib(bytes)
-      if got != want {
-        b.Fatalf("%d(%x) != %d(%x)",got, got, want, want)
-      }
-    }
-  }
+	for i := 0; i < b.N; i++ {
+		bytes := randBytes
+		for _, want := range randInts {
+			var got uint32
+			bytes, got = lib(bytes)
+			if got != want {
+				b.Fatalf("%d(%x) != %d(%x)", got, got, want, want)
+			}
+		}
+	}
 }
 
 func BenchmarkPlain(b *testing.B) {
-  for i:=0; i<b.N; i++ {
-    bytes := randBytes
-    for _, want := range(randInts) {
-      var got uint32
-      bytes, got = plain(bytes)
-      if got != want {
-        b.Fatalf("%d(%x) != %d(%x)",got, got, want, want)
-      }
-    }
-  }
+	for i := 0; i < b.N; i++ {
+		bytes := randBytes
+		for _, want := range randInts {
+			var got uint32
+			bytes, got = plain(bytes)
+			if got != want {
+				b.Fatalf("%d(%x) != %d(%x)", got, got, want, want)
+			}
+		}
+	}
 }
 
 func BenchmarkClean(b *testing.B) {
-  for i:=0; i<b.N; i++ {
-    bytes := randBytes
-    for _, want := range(randInts) {
-      var got uint32
-      bytes, got = clean(bytes)
-      if got != want {
-        b.Fatalf("%d(%x) != %d(%x)",got, got, want, want)
-      }
-    }
-  }
+	for i := 0; i < b.N; i++ {
+		bytes := randBytes
+		for _, want := range randInts {
+			var got uint32
+			bytes, got = clean(bytes)
+			if got != want {
+				b.Fatalf("%d(%x) != %d(%x)", got, got, want, want)
+			}
+		}
+	}
 }
 
 func BenchmarkMagic(b *testing.B) {
-  for i:=0; i<b.N; i++ {
-    bytes := randBytes
-    for _, want := range(randInts) {
-      var got uint32
-      bytes, got = magic(bytes)
-      if got != want {
-        b.Fatalf("%d(%x) != %d(%x)",got, got, want, want)
-      }
-    }
-  }
+	for i := 0; i < b.N; i++ {
+		bytes := randBytes
+		for _, want := range randInts {
+			var got uint32
+			bytes, got = magic(bytes)
+			if got != want {
+				b.Fatalf("%d(%x) != %d(%x)", got, got, want, want)
+			}
+		}
+	}
+}
+
+func BenchmarkMagic2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bytes := randBytes
+		for _, want := range randInts {
+			var got uint32
+			bytes, got = magic2(bytes)
+			if got != want {
+				b.Fatalf("%d(%x) != %d(%x)", got, got, want, want)
+			}
+		}
+	}
+}
+
+func BenchmarkMagic3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		bytes := randBytes
+		for _, want := range randInts {
+			var got uint32
+			bytes, got = magic3(bytes)
+			if got != want {
+				b.Fatalf("%d(%x) != %d(%x)", got, got, want, want)
+			}
+		}
+	}
 }
